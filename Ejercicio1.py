bfce561540e263388d2a47603e59ef4d623994eb
Sassier <sassier@gmail.com>
2023-11-07 10:18:46 +0100

    Ejercicio1

---
+++ Ejercicio1.py
@@ -0,0 +1,3 @@
+UserName = input("Introduce el nombre de usuario \n")
+UserAge = int(input("Introduce la edad del usuario \n"))
+print("El nombre es: " + UserName +" y su edad es: " + str(UserAge) + " años que al cambio son: " + str((UserAge * 360)) + " dias")
